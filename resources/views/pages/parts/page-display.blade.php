381be4f073b70998e7977dcda7f9a84fd0d7621a
sgrjr <sgrjr@deliverance.me>
2021-09-17 22:30:40 -0700

    server

--- resources/views/pages/parts/page-display.blade.php
+++ resources/views/pages/parts/page-display.blade.php
@@ -1,10 +1,6 @@
-<div dir="auto" id="quiz">
+<div dir="auto">
 
-    <div style="clear:left;"></div>
-    
-    <div class="chapter chapter-expansion">
-        
-        
+    <div id="quiz" class="chapter chapter-expansion">
         <div class="content">
             <button type="button" chapter-toggle="" aria-expanded="false" class="text-muted chapter-expansion-toggle"><svg class="svg-icon" data-icon="caret-right" role="presentation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9.5 17.5l5-5-5-5z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg> <span>Review</span></button>
             <div class="inset-list">
@@ -14,18 +10,23 @@
                             <div class="question"></div>
                             <input class="answered"></input>
                             <button onClick="score()">score</button>
+                            <button onClick="skip()">skip</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <h1 class="break-text" id="bkmrk-page-title">{{$page->name}}</h1>
+
+    <div style="clear:left;"></div>
 
     @if (isset($diff) && $diff)
         {!! $diff !!}
     @else
         {!! isset($page->renderedHTML) ? $page->renderedHTML : $page->html !!}
     @endif
-    
+
+
 </div>
